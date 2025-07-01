78585427d1960701364ce90d93e4f14b315fa87c
Laurence Cadag <laurencecadag1@gmail.com>
2025-07-01 08:53:51 +0800

    Environment based dbClient setup

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
+
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-const client = new DynamoDBClient({
-  region: "ap-southeast-1",
-  credentials: {
-    accessKeyId: "fakeuser",
-    secretAccessKey: "fakerkey",
-  },
-  endpoint: "http://localhost:8000", // DynamoDB Local
-});
+const isOffline = process.env.IS_OFFLINE === "true";
+
+const client = new DynamoDBClient(
+  isOffline
+    ? {
+        region: "ap-southeast-1",
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        },
+        endpoint: process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
+      }
+    : {
+        region: "ap-southeast-1",
+      }
+);
 
 export default client;
