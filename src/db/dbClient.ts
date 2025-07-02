fe0aab89319fdbb9835259845c465a5f1fb6723e
Laurence Cadag <laurencecadag1@gmail.com>
2025-07-02 14:34:42 +0800

    AWS Deployment setup change

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -1,25 +1,19 @@
-import "dotenv/config";
-
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-const isOffline = true;
+const isOffline = process.env.IS_OFFLINE === "true";
 
 const client = new DynamoDBClient(
-  !isOffline
+  isOffline
     ? {
-        region: "ap-southeast-1",
-        credentials: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-        },
-      }
-    : {
         region: "ap-southeast-1",
         credentials: {
           accessKeyId: "fakeuser",
           secretAccessKey: "fakerkey",
         },
         endpoint: process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
+      }
+    : {
+        region: "ap-southeast-1",
       }
 );
 
