a1a38621d96063d71af46122dc1dc6877c2ecd21
Laurence Cadag <laurencecadag1@gmail.com>
2025-07-02 08:48:24 +0800

    DbClient conditional setup

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -2,20 +2,24 @@ import "dotenv/config";
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-const isOffline = process.env.IS_OFFLINE === "true";
+const isOffline = true;
 
 const client = new DynamoDBClient(
-  isOffline
+  !isOffline
     ? {
         region: "ap-southeast-1",
         credentials: {
           accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
           secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
         },
-        endpoint: process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
       }
     : {
         region: "ap-southeast-1",
+        credentials: {
+          accessKeyId: "fakeuser",
+          secretAccessKey: "fakerkey",
+        },
+        endpoint: process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
       }
 );
 
