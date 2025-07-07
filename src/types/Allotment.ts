e455523125c93bbef84559f5d0007ed9462b746d
Laurence Cadag <laurencecadag1@gmail.com>
2025-07-07 10:31:44 +0800

    Allotment Interface

--- src/types/Allotment.ts
+++ src/types/Allotment.ts
@@ -1,7 +1,32 @@
+export interface AllotmentFilters {
+  status?: string;
+  createdAtRange?: { from: string; to: string };
+  papId?: string;
+  allotmentId?: string;
+  search?: string;
+  particulars?: string;
+}
+
+export interface AllotmentBreakdown {
+  fieldOfficeId: string;
+  papId: string;
+  uacsId: string;
+  amount: number;
+}
+
 export interface Allotment {
   officeId: string;
   allotmentId: string;
-  amount: number;
-  year: number;
-  [key: string]: any;
+  particulars: string;
+  appropriationType: string;
+  bfarsBudgetType: string;
+  allotmentType: string;
+  status:
+    | "for-triage"
+    | "for-processing"
+    | "for-peer-review"
+    | "for-approval"
+    | "approved-and-posted"
+    | "completed";
+  breakdown: AllotmentBreakdown[];
 }
