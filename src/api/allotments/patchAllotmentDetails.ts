d6dc77599a7c969aaa17710a0bfdf9de1816a3ff
Laurence Cadag <laurencecadag1@gmail.com>
2025-07-18 09:54:15 +0800

    Update Allotment Details Function

---
+++ src/api/allotments/patchAllotmentDetails.ts
@@ -0,0 +1,45 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { allotmentService } from "../../services/allotmentService";
+
+const { patchDetails } = allotmentService();
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  try {
+    const allotmentId = event.pathParameters?.allotmentId;
+    if (!allotmentId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Missing allotmentId in path parameters",
+        }),
+      };
+    }
+
+    const body = JSON.parse(event.body || "{}");
+
+    if (!body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Invalid body. Missing required fields.",
+        }),
+      };
+    }
+
+    const result = await patchDetails(allotmentId, body);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Allotment details updated successfully",
+        data: result,
+      }),
+    };
+  } catch (error) {
+    console.error("Error patching allotment details:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
+};
